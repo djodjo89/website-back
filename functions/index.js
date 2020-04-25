7166c93c57825ecfb16f2de88f9682a7e4eed3f9
djodjo89 <mathysimion@yahoo.fr>
2020-04-25 09:42:11 +0200

    WEBB-2-3 chore(archi): use google auth for authentication

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,36 @@
 const cors = require('cors')({ origin: true });
 const mailer = require('nodemailer');
 const functions = require('firebase-functions');
+const { google } = require('googleapis');
 const admin = require('firebase-admin');
 const validator = require('node-email-validation');
 const Entities = require('html-entities').AllHtmlEntities;
 
 admin.initializeApp();
+const OAuth2 = google.auth.OAuth2;
 const entities = new Entities();
 const env = functions.config();
 
+const oauth2Client = new OAuth2(
+    env.oauth.client.id,
+    env.oauth.client.secret,
+    env.oauth.playground.url,
+);
+
+oauth2Client.setCredentials({
+    refresh_token: env.oauth.token.refresh,
+});
+const accessToken = oauth2Client.getAccessToken();
+
 const transport=mailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    service: 'gmail',
     auth: {
-        user: env.mail.address,
-        pass: env.mail.passwd,
+        type: 'OAuth2',
+        user: env.user.email,
+        clientId: env.oauth.client.id,
+        clientSecret: env.oauth.client.secret,
+        refreshToken: env.oauth.token.refresh,
+        accessToken: accessToken,
     },
 });
 
@@ -45,7 +60,7 @@ exports.contact = functions.https.onRequest((req, res) => {
         if (correctEmail && !emptySubject) {
             const options = {
                 from: `${name} - <${email}>`,
-                to: env.mail.address,
+                to: env.user.email,
                 subject: `${subject}`,
                 text: content,
             };

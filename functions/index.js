d573d60b9962f08fa58458d1cefdba4a33b7630d
djodjo89 <mathysimion@yahoo.fr>
2020-04-25 06:38:06 +0200

    WEBB-2-2 fix(archi): deploy to firebase

--- functions/index.js
+++ functions/index.js
@@ -1,76 +1,64 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const cors = require('cors')({ origin: true });
 const mailer = require('nodemailer');
-const helmet = require('helmet');
-const compression = require('compression');
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const validator = require('node-email-validation');
 const Entities = require('html-entities').AllHtmlEntities;
 
-const app = express();
+admin.initializeApp();
 const entities = new Entities();
 const env = functions.config();
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(helmet());
-app.use(compression());
-
-app.post('/contact', (req, res) => {
-    const isString = variable => typeof variable === 'string';
-    const message = {};
-    const encodeBody = _ => Object.entries(req.body)
-        .filter(entry => isString(entry[0]) && isString(entry[0]))
-        .map(entry => entry.map(entities.encode))
-        .forEach(entry => message[entry[0]] = entry[1]);
-    encodeBody();
-    const {
-        name,
-        email,
-        subject,
-        content
-    } =  message;
-    console.log(message);
-    const correctEmail = validator.is_email_valid(email);
-    const emptySubject = subject === '';
-
-    if (correctEmail && !emptySubject) {
-        const transport=mailer.createTransport({
-            host: 'smtp.gmail.com',
-            service: 'Gmail',
-            port: 465,
-            secure: true,
-            requireTLS: true,
-            auth: {
-                user: env.mail.address,
-                pass: env.mail.passwd,
-            },
-        });
-
-        const options = {
-            from: `${name} - <${email}>`,
-            to: env.mail.address,
-            subject: `${subject}`,
-            text: content,
-        };
-
-        transport.sendMail(options, error =>
-            error
-                ? res.json(`Email could not be sent due to error ${error}`)
-                : res.json('Email has been sent successfully')
-        );
-    } else if (!correctEmail) {
-        res.json(422, `Email address ${email} is incorrect`);
-    } else if (emptySubject) {
-        res.json(422, 'Error, subject is empty');
-    }
+const transport=mailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: env.mail.address,
+        pass: env.mail.passwd,
+    },
 });
 
-app.listen(env.app.port, () => console.log(`Listening on port ${env.app.port}`));
+exports.contact = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        const isString = variable => typeof variable === 'string';
+        const message = {};
+        const encodeBody = _ => Object.entries(req.body)
+            .filter(entry => isString(entry[0]) && isString(entry[0]))
+            .map(entry => entry.map(entities.encode))
+            .forEach(entry => message[entry[0]] = entry[1]);
 
-const contact = functions.https.onRequest(app);
+        encodeBody();
 
-module.exports = {
-    contact,
-};
+        const {
+            name,
+            email,
+            subject,
+            content
+        } =  message;
+
+        console.log('Message', message);
+
+        const correctEmail = validator.is_email_valid(email);
+        const emptySubject = subject === '';
+
+        if (correctEmail && !emptySubject) {
+            const options = {
+                from: `${name} - <${email}>`,
+                to: env.mail.address,
+                subject: `${subject}`,
+                text: content,
+            };
+
+            transport.sendMail(options, error =>
+                error
+                    ? res.json(`Email could not be sent due to error ${error}`)
+                    : res.json('Email has been sent successfully')
+            );
+        } else if (!correctEmail) {
+            res.json(`Email address ${email} is incorrect`);
+        } else if (emptySubject) {
+            res.json('Error, subject is empty');
+        }
+    });
+});
